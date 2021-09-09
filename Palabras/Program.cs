8fe66f044d94aafd762c6255c2d978730965bc1d
Pablo Blanco Gil <pblanco@hiberus.com>
2021-09-09 13:11:36 +0200

    Agregar archivos de proyecto.

---
+++ Palabras/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palabras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
