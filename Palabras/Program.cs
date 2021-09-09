2740ec19a23d561bb494218d2f667904d69c1e91
Pablo Blanco Gil <pblanco@hiberus.com>
2021-09-09 13:25:21 +0200

    Repositorio

--- Palabras/Program.cs
+++ Palabras/Program.cs
@@ -6,9 +6,16 @@ namespace Palabras
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Escribe una palabra o la frase que te venga a la cabeza : ");
+            Console.WriteLine(" ");
+            string palabrita = Console.ReadLine();
 
+            for (int i = 0; i < palabrita.Length; i++)
+            {
+                System.Console.Write(palabrita[palabrita.Length - i - 1]);
+                Console.WriteLine(" ");
+
+            }
         }
     }
 }
